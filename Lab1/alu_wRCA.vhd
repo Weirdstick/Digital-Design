c6dc8775f6cc71aac79a8710d59bd082703da951
Weirdstick <irene.mona.d0@gmail.com>
2025-02-05 05:39:39 +0100

    Update alu_wRCA.vhd
    comments describing parts of the code
    
    Signed-off-by: Weirdstick <irene.mona.d0@gmail.com>

--- Lab1/alu_wRCA.vhd
+++ Lab1/alu_wRCA.vhd
@@ -2,6 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+-- Interface declarations
 entity alu_wRCA is
     port(
         alu_inA, alu_inB : in  std_logic_vector(7 downto 0);
@@ -13,6 +14,7 @@ entity alu_wRCA is
     );
 end alu_wRCA;
 
+-- Describes expected behaviour for struct and components
 architecture structural of alu_wRCA is
     component rca
         port(
@@ -36,15 +38,16 @@ architecture structural of alu_wRCA is
     signal cmp_result                  : std_logic;
     
 begin
-    -- Subtraction Logic (2's complement)
-    sub_b   <= not alu_inB when alu_op = "11" else alu_inB;
-    cin_sub <= '1' when alu_op = "11" else '0';
+    -- Subtraction (2's complement)
+    sub_b   <= not alu_inB when alu_op = "11" else alu_inB; -- Invert
+    cin_sub <= '1' when alu_op = "11" else '0'; -- Add 1 by pre-loading c_in
+    
 
-    -- Adder/Subtractor Unit
+    -- Adder/Subtractor 
     ADDER: rca port map(
         a    => alu_inA,
         b    => sub_b,
-        cin  => cin_sub,
+        cin  => cin_sub, -- Set as 1 when instruction b'11 is called
         cout => carry_flag,
         s    => adder_out
     );
@@ -55,14 +58,14 @@ begin
     -- ROL Operation (1-bit left rotation)
     rol_out <= alu_inA(6 downto 0) & alu_inA(7);
 
-    -- Comparator Unit (Always active)
+    -- Comparator Unit (Always active, will always output result hence not included in multiplexer)
     CMP_UNIT: cmp port map(
         a => alu_inA,
         b => alu_inB,
         e => cmp_result
     );
 
-    -- Output MUX
+    -- Output MUX (Multiplexer)
     with alu_op select mux_out <=
         rol_out    when "00",  -- ROL
         and_out    when "01",  -- AND
@@ -75,6 +78,6 @@ begin
     C <= not carry_flag when alu_op = "11" else  -- Invert carry for borrow
          carry_flag when alu_op = "10" else      -- Direct carry for ADD
          '0';
-    E <= cmp_result;  -- Always output comparison result
+    E <= cmp_result;  -- Always output comp result
     Z <= '1' when mux_out = "00000000" else '0';
 end structural;
